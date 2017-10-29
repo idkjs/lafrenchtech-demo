9da2adeeacbd55f462573e9de2c1e7163e2fb080
idkjs <aarmand.inbox@gmail.com>
2017-10-29 16:11:48 +0100

    works. xray, failing

--- lafrenchtech-client/src/components/Company.js
+++ lafrenchtech-client/src/components/Company.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import { createFragmentContainer, graphql } from "react-relay";
 
-import { Card, Divider, Pre, Image } from "rebass";
+import { Container, Card, Divider, Pre, Image, Link } from "rebass";
 
 class Company extends Component {
   render() {
     return (
-      <Card p={2} height="192" overflow="hidden">
-        <Divider color="gray2" />
-        <Image src={this.props.company.logo} />
-        <Pre f={0} children={this.props.company.name}>
-          {this.props.company.name}
+      <Container>
+        <Card p={2} height="192" overflow="hidden">
+          <Divider color="gray2" />
+          <Image src={this.props.company.logo} />
+          <Divider color="gray2" />
+        </Card>
+        <Pre p={2} children={this.props.company.name}>
+          <Link href={this.props.company.url}>{this.props.company.name}</Link>
         </Pre>
-      </Card>
+      </Container>
     );
   }
 }
