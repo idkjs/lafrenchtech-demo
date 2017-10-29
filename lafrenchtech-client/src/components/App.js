a5b2466c71eb6f88daebea5bc884ede566d91256
idkjs <aarmand.inbox@gmail.com>
2017-10-29 15:53:18 +0100

    better, links not working

--- lafrenchtech-client/src/components/App.js
+++ lafrenchtech-client/src/components/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import { QueryRenderer, graphql } from "react-relay";
-import styled from "styled-components";
 import ListPage from "./ListPage";
 import environment from "../Environment";
-import { mainColor, Title } from "./Styles";
 import Corner from "./Corner";
-import PageTitle from "./PageTitle";
 
 const AppListPageQuery = graphql`
   query AppListPageQuery {
@@ -20,9 +17,6 @@ class App extends Component {
     return (
       <div className={this.props.className}>
         <Corner />
-        <PageTitle href={"http://visa.lafrenchtech.com"}>
-          LaFrenchTech
-        </PageTitle>
         <QueryRenderer
           environment={environment}
           query={AppListPageQuery}
--- lafrenchtech-client/src/components/Company.js
+++ lafrenchtech-client/src/components/Company.js
@@ -1,26 +1,17 @@
 import React, { Component } from "react";
 import { createFragmentContainer, graphql } from "react-relay";
 
-import {
-  Card,
-  Border,
-  Box,
-  BackgroundImage,
-  Subhead,
-  Small,
-  Link
-} from "rebass";
+import { Card, Divider, Pre, Image } from "rebass";
+
 class Company extends Component {
   render() {
     return (
-      <Card width={256}>
-        <Border py={2} top bottom>
-          <BackgroundImage ratio={1} src={this.props.company.logo} />
-          <Box p={2}>
-            <Link href={this.props.company.url}>{this.props.company.name}</Link>
-            <Small>Small meta text</Small>
-          </Box>
-        </Border>
+      <Card p={2} height="192" overflow="hidden">
+        <Divider color="gray2" />
+        <Image src={this.props.company.logo} />
+        <Pre f={0} children={this.props.company.name}>
+          {this.props.company.name}
+        </Pre>
       </Card>
     );
   }
