1a3c802ce79a595bc07206fce5114cb6241cce35
idkjs <aarmand.inbox@gmail.com>
2017-10-29 17:08:15 +0100

    clean up after xray try

--- lafrenchtech-client/src/components/ListPage.js
+++ lafrenchtech-client/src/components/ListPage.js
@@ -3,23 +3,21 @@ import { createFragmentContainer, graphql } from "react-relay";
 import { Flex } from "rebass";
 import PageTitle from "./PageTitle";
 import Company from "./Company";
-import XRay from "react-x-ray";
+
 class ListPage extends Component {
   render() {
     // console.log(this.props.companies);
     return (
-      <XRay>
-        <div>
-          <PageTitle p={4}>
-            {this.props.companies.edges.length} Companies
-          </PageTitle>
-          <Flex wrap my={2} mx={-2}>
-            {this.props.companies.edges.map(({ node }, index) => (
-              <Company key={node.__id} index={index} company={node} />
-            ))}
-          </Flex>
-        </div>
-      </XRay>
+      <div>
+        <PageTitle p={4}>
+          {this.props.companies.edges.length} Companies
+        </PageTitle>
+        <Flex wrap my={2} mx={-2}>
+          {this.props.companies.edges.map(({ node }, index) => (
+            <Company key={node.__id} index={index} company={node} />
+          ))}
+        </Flex>
+      </div>
     );
   }
 }
