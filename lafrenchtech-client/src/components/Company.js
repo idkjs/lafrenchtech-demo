2e3683d6cd3aa62b7d27060f4b2a4c40edd74bfb
idkjs <aarmand.inbox@gmail.com>
2017-10-29 22:13:10 +0100

    box 1/3

--- lafrenchtech-client/src/components/Company.js
+++ lafrenchtech-client/src/components/Company.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { createFragmentContainer, graphql } from "react-relay";
 
-import { Flex, Card, Divider, Box, Image, Link } from "rebass";
+import { Flex, Card, Divider, Box, Grid, Image, Link } from "rebass";
 
 class Company extends Component {
   render() {
     return (
-      <Box p={3} width={[1, 1 / 2]}>
+      <Box p={3} width={[1, 1 / 2, 1 / 3]}>
         <Image src={this.props.company.logo} />
         <Divider color="black" />
         <Link href={this.props.company.url}>{this.props.company.name}</Link>
